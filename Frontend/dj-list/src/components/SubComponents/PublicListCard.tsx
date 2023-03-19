875aa9845ad6aac4315f665e318c63aaecf828e6
ArxLancifer <a_christoforidis@hotmail.com>
2023-03-19 09:06:10 +0200

    Like button and stylings

--- Frontend/dj-list/src/components/SubComponents/PublicListCard.tsx
+++ Frontend/dj-list/src/components/SubComponents/PublicListCard.tsx
@@ -3,7 +3,7 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
 import { IPublicListCard } from '../../interfaces/UserInterfaces';
-
+import {HandThumbsUp, HandThumbsUpFill} from 'react-bootstrap-icons';
 function PublicListCard({listData}:{listData:IPublicListCard}) {
 
     function dateFormate(date:string){
@@ -11,8 +11,19 @@ function PublicListCard({listData}:{listData:IPublicListCard}) {
         return formatedDate;
     }
 
+    function removedOutline(e:React.SyntheticEvent<HTMLButtonElement>){
+        const buttonClasses = e.currentTarget.classList;
+        if(buttonClasses.contains("btn-outline-primary")){
+            e.currentTarget.classList.remove("btn-outline-primary")
+            e.currentTarget.classList.add("btn-primary")
+        }else{
+            e.currentTarget.classList.remove("btn-primary")
+            e.currentTarget.classList.add("btn-outline-primary")
+        }
+    }
+
   return (
-      <Card className='h-100 m-2' style={{ width: '12rem' }}>
+      <Card className='h-100 m-2' style={{ width: '14rem' }}>
         <Card.Header><small>{listData.user.username}</small></Card.Header>
       <Card.Img className='w-100 rounded-0' variant="top" src={listData.user.userimage || "http://placekitten.com/400/400"} />
       <Card.Body className=' px-2 py-1'>
@@ -21,9 +32,13 @@ function PublicListCard({listData}:{listData:IPublicListCard}) {
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </Card.Text> */}
+        <div className='d-flex justify-content-between'>
         <Link to={`/trackstable/${listData._id}`}>
-        <Button className='py-0 px-1' size='sm' variant="primary">Watch</Button>
+        <Button className='py-1 px-1' size='sm' variant="info">Watch</Button>
+           
         </Link>
+        <button onClick={removedOutline} className='py-1 px-1 btn btn-sm btn-outline-primary' >Like<HandThumbsUp className='fs-6 mb-1 align-middle'/></button>
+         </div>
         <Card.Footer className="text-muted p-0 pt-1 mt-1 bg-light bg-transparent">
         <small className="list-date text-muted">Created at : {dateFormate(listData.createdAt)}</small>
         </Card.Footer>
