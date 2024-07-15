6bf768ef12de32e8c68fd51cbe2ddd361aea3028
ArxLancifer <a_christoforidis@hotmail.com>
2024-07-15 21:44:29 +0300

    fix vulnerabilities

--- Backend/config/mongoDB.js
+++ Backend/config/mongoDB.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectMongo = async function () {
     try {
-        await mongoose.connect(process.env.DB_STRING)
+        await mongoose.connect(process.env.DB_STRING, {useNewUrlParser: true})
         console.log("Connected to MongoDB")
     } catch (error) {
         console.log(error)
